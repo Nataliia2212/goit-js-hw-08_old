b763c653f69df5e3a35a085ef9d00fd3aa09da8e
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2023-04-01 20:03:07 +0200

    nnn

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+console.log(SimpleLightbox)
 console.log(galleryItems);
+
+
+const galleryRef = document.querySelector('.gallery')
+const gallery = createGaleryMarkup(galleryItems);
+
+
+galleryRef.insertAdjacentHTML('beforeend', gallery)
+
+function createGaleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}"/>
+                </a>
+            </li>`    
+    }
+    ).join('')
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {captionsData:	'alt', captionDelay: 250, captionPosition: 'bottom'});
+
+
+
+
+
+console.log('ok')
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,69 @@
+import Vimeo from '@vimeo/player';
+import Throttle from 'lodash.throttle';
 
+console.log(Vimeo)
+
+const iframe = document.querySelector('iframe');
+
+console.log(iframe)
+console.log('gggg')
+
+// iframe.addEventListener('click', onClickImg)
+
+const player = new Vimeo(iframe);
+
+console.log('jjj')
+
+player.on('timeupdate', play);
+
+const sec = localStorage.getItem(Number("videoplayer-current-time"))
+
+ startVideo()
+
+function play(event) {
+    console.log('played the video!');
+
+// if ()
+
+    const videoplayer = event.seconds;
+    localStorage.setItem("videoplayer-current-time", Throttle(videoplayer, 2000))
+    console.dir(videoplayer)
+}
+
+console.log('kakakmn,mn,mn,maka')
+// player.setCurrentTime(sec).then(function(seconds) {
+//    console.log(sec) // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
+
+
+
+
+function startVideo() {
+    player.setCurrentTime(sec).then(function(seconds) {
+   console.log(sec) // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+}
+
+
+// console.log('ok')
